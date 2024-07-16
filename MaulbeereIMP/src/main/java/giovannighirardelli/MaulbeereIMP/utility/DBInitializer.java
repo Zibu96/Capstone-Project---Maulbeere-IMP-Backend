859061d0e91ddf609da363c76874a6885d265185
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-07-16 14:58:57 +0200

    minor fix

--- MaulbeereIMP/src/main/java/giovannighirardelli/MaulbeereIMP/utility/DBInitializer.java
+++ MaulbeereIMP/src/main/java/giovannighirardelli/MaulbeereIMP/utility/DBInitializer.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class DBInitializer {
     @Autowired
     private UserRepository userRepository;
